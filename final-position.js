ed58c54645ee607909eb1170642057b049846895
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-04 17:30:41 -0500

    changed line positions

--- final-position.js
+++ final-position.js
@@ -4,20 +4,20 @@ const finalPosition = function (moves) {
   let position = [0, 0]; // declare starting point
   for (var i = 0; i < moves.length; i++){ // loop from 0, until the end of array, +1 each time
     if (moves[i] == 'north') {
-    position[1]++; //change position acording to index [index] ++ add to
-  }
+      position[1]++; //change position acording to index [index] ++ add to
+    }
     else if (moves[i] == 'west') {
-    position[0]--;
-  }
+      position[0]--;
+    }
     else if (moves[i] == 'east') {
-    position[0]++;
-  }
+      position[0]++;
+    }
     else if (moves[i] == 'south') {
-    position[1]--;
-  }
-  console.log(position);
+      position[1]--;
+    }
+    console.log(position);
   
-}
+  }
 return position; // returns the value of the loop outside the function
 }
 
