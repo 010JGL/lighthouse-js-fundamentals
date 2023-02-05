a411030bbad4fa4459e61ead7a8a15e8c06b4df6
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-05 15:41:23 -0500

    Ice cream exercise

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer"); // Adding root beer at the end of array
+
+console.log(iceCreamFlavours[0]); //show first item of array
+
+console.log(iceCreamFlavours[5]); // shows last item of array
+
+console.log(iceCreamFlavours.length);
