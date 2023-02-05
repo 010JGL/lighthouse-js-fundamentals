4476ffede1c0d94dde76ab57db93565039003d5d
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-05 15:50:14 -0500

    packing list exercise part 2

---
+++ packing-list.js
@@ -0,0 +1,12 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+
+let  i = 0;
+while (i < packingList.length) {  // Same thing as the for loop in comments under
+  console.log(packingList[i]);
+  i++
+}
+/*for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+} */       
