d9cc175f1cd788552a0af493f55be0dd861bd9b3
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-06 16:49:09 -0500

    concat exercise

---
+++ concat.js
@@ -0,0 +1,12 @@
+function concat(arr1, arr2) { // takes the 2 values we need in the function
+  let array = arr1.concat(arr2); // add them together with concat
+  return array; //returns the value we want out of the function
+}
+
+
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
