2b496fffa0652d71f80adc6152cf56f7f72f9e14
Jeremy G.Lafleur <010JGL@gmail.com>
2023-02-07 14:12:20 -0500

    object keys exercise

---
+++ object-keys.js
@@ -0,0 +1,8 @@
+const car = {
+  make: 'Honda',
+  model: 'Accord',
+  year: 2020
+}
+const keys = Object.keys(car); // make a constant to regroup all of the car object
+
+console.log(keys); // can now print all object at once
